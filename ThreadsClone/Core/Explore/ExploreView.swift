e1ecd7a9f0b2f28086d76aaf0108b2e1ecee7cda
Sasha Jaroshevskii 🦄 <72662383+jaroshvekii@users.noreply.github.com>
2025-10-27 21:03:38 +0100

    Add profile view

--- ThreadsClone/Core/Explore/ExploreView.swift
+++ ThreadsClone/Core/Explore/ExploreView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ExploreView: View {
+  @State private var searchText = ""
+  
   var body: some View {
-    Text("Explore")
+    ScrollView {
+      LazyVStack {
+        ForEach(0..<10, id: \.self) { user in
+          VStack {
+            UserCell()
+            
+            Divider()
+          }
+          .padding(.vertical, 4)
+        }
+      }
+    }
+    .navigationTitle("Search")
+    .searchable(text: $searchText, prompt: "Search")
   }
 }
 
 #Preview {
-  ExploreView()
+  NavigationStack {
+    ExploreView()
+  }
 }
--- ThreadsClone/Core/Feed/FeedView.swift
+++ ThreadsClone/Core/Feed/FeedView.swift
@@ -43,11 +43,7 @@ struct ThreadCell: View {
   var body: some View {
     VStack {
       HStack(alignment: .top) {
-        Image(.avatarSample)
-          .resizable()
-          .scaledToFill()
-          .frame(width: 40, height: 40)
-          .clipShape(.circle)
+        CircularProfileImageView()
         
         VStack(alignment: .leading, spacing: 4) {
           HStack {
