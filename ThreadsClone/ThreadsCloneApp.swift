a808ca82b18aeb6b06001fbbe7cda3d3bf1e54f8
Sasha Jaroshevskii 🦄 <72662383+jaroshvekii@users.noreply.github.com>
2025-10-27 15:14:44 +0100

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Sasha Jaroshevskii on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsCloneTests/ThreadsCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneTests.swift
+//  ThreadsCloneTests
+//
+//  Created by Sasha Jaroshevskii on 10/27/25.
+//
+
+import Testing
+@testable import ThreadsClone
+
+struct ThreadsCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
