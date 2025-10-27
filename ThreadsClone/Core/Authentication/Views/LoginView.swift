d48ec552a7467866014ff9b390786b79e2ac30fe
Sasha Jaroshevskii 🦄 <72662383+jaroshvekii@users.noreply.github.com>
2025-10-27 15:23:11 +0100

    Initial commit

---
+++ ThreadsClone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsClone
+//
+//  Created by Sasha Jaroshevskii on 10/27/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
